bb618d898cca7b13e13dc809c41d5f187035c331
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-23 08:39:49 -0300

    fix : remove depends_on, wait-for-it add

---
+++ wait-for-it.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+host=$(echo $1 | cut -d: -f1)
+port=$(echo $1 | cut -d: -f2)
+shift
+
+timeout=15
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -t|--timeout)
+    timeout="$2"
+    shift
+    shift
+    ;;
+    --)
+    shift
+    break
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+echo "Waiting for $host:$port to be available..."
+
+for ((i=0; i<timeout; i++)); do
+    if nc -z $host $port; then
+        echo "$host:$port is available!"
+        exec "$@"
+        exit 0
+    fi
+    sleep 1
+done
+
+echo "Timeout waiting for $host:$port"
+exit 1
