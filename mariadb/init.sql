bff81427aa141f5f6d1845ffb5e7c73ef087ca2a
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-23 16:27:27 -0300

    feat : db init sql add

---
+++ mariadb/init.sql
@@ -0,0 +1,3 @@
+-- ./mariadb/init.sql
+CREATE DATABASE IF NOT EXISTS maplekiosk CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
