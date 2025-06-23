a2cc2e75f57174dabdad2893b73fd56df06fd766
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-23 09:01:12 -0300

    fix : change wait-for-it for alpine image

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,40 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 
-host=$(echo $1 | cut -d: -f1)
-port=$(echo $1 | cut -d: -f2)
+host=$(echo "$1" | cut -d: -f1)
+port=$(echo "$1" | cut -d: -f2)
 shift
 
 timeout=15
-while [[ $# -gt 0 ]]
-do
-key="$1"
-case $key in
+while [ "$#" -gt 0 ]; do
+  key="$1"
+  case $key in
     -t|--timeout)
-    timeout="$2"
-    shift
-    shift
-    ;;
+      timeout="$2"
+      shift
+      shift
+      ;;
     --)
-    shift
-    break
-    ;;
+      shift
+      break
+      ;;
     *)
-    shift
-    ;;
-esac
+      shift
+      ;;
+  esac
 done
 
 echo "Waiting for $host:$port to be available..."
 
-for ((i=0; i<timeout; i++)); do
-    if nc -z $host $port; then
-        echo "$host:$port is available!"
-        exec "$@"
-        exit 0
-    fi
-    sleep 1
+i=0
+while [ "$i" -lt "$timeout" ]; do
+  if nc -z "$host" "$port"; then
+    echo "$host:$port is available!"
+    exec "$@"
+    exit 0
+  fi
+  i=$((i + 1))
+  sleep 1
 done
 
 echo "Timeout waiting for $host:$port"
