70c0f714f3e863523e140adfa4bc2b57f0b591c3
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-23 21:12:20 -0300

    fix : Naming change according to command change

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -1,3 +1,3 @@
 -- ./mariadb/init.sql
-CREATE DATABASE IF NOT EXISTS maplekiosk CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS maple_kiosk CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS maple_auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
